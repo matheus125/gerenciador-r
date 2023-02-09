0d0c53f4f07c08fca3c879aa306dbcf2feb729a0
Matheu Mota da Silva <matheus125_mota@hotmail.com>
2023-02-09 13:55:00 -0400

    ultimas atualizações

--- admin-projetos.php
+++ admin-projetos.php
@@ -1,97 +1,142 @@
-<?php 
+<?php
 
-use \Hcode\PageAdminProjetos;
-use \Hcode\Model\User;
 use \Hcode\Model\Clientes;
 use \Hcode\Model\Projetos;
+use \Hcode\Model\User;
+use \Hcode\PageAdminProjetos;
 
-$app->get("/admin/Projetos/projetos", function(){
+$app->get("/admin/Projetos/projetos", function () {
 
-	User::verifyLogin();
+    User::verifyLogin();
 
-	$page = new PageAdminProjetos();
+    $page = new PageAdminProjetos();
 
-	$page->setTpl("projetos", [
-		'projetos'=>Projetos::listAll()
-	]);
-
-});
-
-$app->get("/admin/Projetos/projeto/create", function(){
-
-	User::verifyLogin();
-
-	$page = new PageAdminProjetos();
-
-	$page->setTpl("projeto-create", [
-		'cliente'=>Clientes::listAll(),
-		'funcionario'=>User::listAll()
-	]);
+    $page->setTpl("projetos", [
+        'projetos' => Projetos::listAll(),
+    ]);
 
 });
 
-$app->post("/admin/Projetos/projeto/create", function(){
+$app->get("/admin/Projetos/projeto/create", function () {
 
-	User::verifyLogin();
+    User::verifyLogin();
 
-	$projetos = new Projetos();
+    $page = new PageAdminProjetos();
 
-	$projetos->setData($_POST);
-
-	$projetos->save();
-
-	header('Location: /admin/Projetos/projetos');
-	exit;
+    $page->setTpl("projeto-create", [
+        'cliente' => Clientes::listAll(),
+        'funcionario' => User::listAll(),
+    ]);
 
 });
 
-$app->get("/admin/Projetos/projetos/:id_projeto", function($id_projeto){
-	
-	User::verifyLogin();
+$app->post("/admin/Projetos/projeto/create", function () {
 
-	$projetos = new Projetos();
+    User::verifyLogin();
 
-	$projetos->get((int)$id_projeto);
+    $projetos = new Projetos();
 
-	$page = new PageAdminProjetos();
+    $projetos->setData($_POST);
 
-	$page->setTpl("projeto-update",[
-	'projetos'=>$projetos->getValues()
-	]);
+    $projetos->save();
 
-});
-
-$app->post("/admin/produtos/:id_projeto", function($id_projeto){
-	
-	User::verifyLogin();
-
-	$produtos = new Produtos();
-
-	$produtos->get((int)$id_projeto);
-	
-	$produtos->setData($_POST);
-	
-	$produtos->update();
-	
-	header("Location: /admin/Projetos/produtos");
-	exit;	
+    header('Location: /admin/Projetos/projetos');
+    exit;
 
 });
 
+$app->get("/admin/Projetos/projetos/:id_projeto", function ($id_projeto) {
 
-$app->get("/admin/Projetos/projetos/:id_projeto/delete", function($id_projeto){
-	
-	User::verifyLogin();
+    User::verifyLogin();
 
-	$projetos = new Projetos();
+    $projetos = new Projetos();
 
-	$projetos->get((int)$id_projeto);
+    $projetos->get((int) $id_projeto);
 
-	$projetos->delete();
+    $page = new PageAdminProjetos();
 
-	header("Location: /admin/Projetos/projetos");
-	exit;
+    $page->setTpl("projeto-update", [
+        'projetos' => $projetos->getValues(),
+    ]);
+});
+
+$app->post("/admin/Projetos/projetos/:id_projeto", function ($id_projeto) {
+
+    User::verifyLogin();
+
+    $projetos = new Projetos();
+
+    $projetos->get((int) $id_projeto);
+
+    $projetos->setData($_POST);
+
+    $projetos->update();
+
+    header("Location: /admin/Projetos/projetos");
+    exit;
 
 });
 
- ?>
+$app->get("/admin/Projetos/projetos/:id_projeto/delete", function ($id_projeto) {
+
+    User::verifyLogin();
+
+    $projetos = new Projetos();
+
+    $projetos->get((int) $id_projeto);
+
+    $projetos->delete();
+
+    header("Location: /admin/Projetos/projetos");
+    exit;
+
+});
+
+$app->get("/admin/Projetos/projetos/projeto/detalhe/:id_projeto", function ($id_projeto) {
+
+    User::verifyLogin();
+
+    $projetos = new Projetos();
+    $tarefa = new Projetos();
+
+    $projetos->get((int) $id_projeto);
+    $tarefa->getid((int) $id_projeto);
+
+    $page = new PageAdminProjetos();
+
+    $page->setTpl("projeto-detalhe", [
+        'projetos' => $projetos->getValues(),
+        'tarefa' => $tarefa->getValues(),
+    ]);
+});
+
+$app->get("/admin/Projetos/tarefas/create/:id_projeto", function ($id_projeto) {
+
+    User::verifyLogin();
+
+    $projetos = new Projetos();
+
+    $projetos->get((int) $id_projeto);
+
+    $page = new PageAdminProjetos();
+
+    $page->setTpl("tarefas-create", [
+        'funcionario' => User::listAll(),
+        'projetos' => $projetos->getValues(),
+    ]);
+});
+
+$app->post("/admin/Projetos/tarefas/create", function () {
+
+    User::verifyLogin();
+
+    $projetos = new Projetos();
+
+    $projetos->setData($_POST);
+
+    $projetos->saveTarefas();
+
+    header('Location: /admin/Projetos/projetos');
+    exit;
+
+});
--- vendor/api/php-classes/src/Model/Projetos.php
+++ vendor/api/php-classes/src/Model/Projetos.php
@@ -13,10 +13,27 @@ class Projetos extends Model
 
         $sql = new Sql();
 
-        return $sql->select("SELECT p.id_projeto, p.nome_projeto, c.idclient, c.company_name, e.employees_name, 
-		p.descricao_projeto, p.estado, p.orcamento_projeto, p.data_inicio_projeto, p.data_fim_projeto 
-		FROM tb_projetos as p inner join tb_client as c on p.idclient = c.idclient 
-		join tb_employees as e on p.id_employees = e.id_employees order by p.nome_projeto");
+        return $sql->select("SELECT p.id_projeto, p.nome_projeto, c.idclient, c.company_name, e.id_employees , e.employees_name,
+		p.descricao_projeto, p.estado, p.progresso, p.orcamento_projeto, p.data_inicio_projeto, p.data_fim_projeto
+		FROM tb_projetos as p inner join tb_client as c on p.idclient = c.idclient
+		join tb_employees as e on p.id_employees = e.id_employees order by p.nome_projeto
+        ");
+    }
+
+    
+    public function getid($id_projeto)
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_atividades a
+        INNER JOIN tb_projetos b ON b.id_projeto = a.id_projeto
+        INNER JOIN tb_employees c ON c.id_employees = a.id_employees
+        WHERE a.id_projeto = :id_projeto order by employees_name
+                ", [
+            ':id_projeto' => $id_projeto
+        ]);
+        $this->setData($results[0]);
     }
 
     public static function checkList($list)
@@ -37,7 +54,7 @@ class Projetos extends Model
 
         $sql = new Sql();
 
-        $results = $sql->select("CALL sp_save_projetos(:nome_projeto, :idclient, :id_employees, :descricao_projeto, 
+        $results = $sql->select("CALL sp_save_projetos(:nome_projeto, :idclient, :id_employees, :descricao_projeto,
         :estado, :orcamento_projeto, :data_inicio_projeto, :data_fim_projeto)", array(
             ":nome_projeto" => $this->getnome_projeto(),
             ":idclient" => $this->getidclient(),
@@ -48,10 +65,24 @@ class Projetos extends Model
             ":data_inicio_projeto" => $this->getdata_inicio_projeto(),
             ":data_fim_projeto" => $this->getdata_fim_projeto(),
         ));
+        
+    }
 
-        // die(var_dump($this->getnamecourses(), $this->getdesurl(), $this->getdscription()));
+    public function saveTarefas()
+    {
 
-        // $this->setData($results[0]);
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_save_tarefas(:id_projeto, :id_employees, :tarefa, :descricao,
+        :data_inicio_tarefa, :data_fim_tarefa)", array(
+            ":id_projeto" => $this->getid_projeto(),
+            ":id_employees" => $this->getid_employees(),
+            ":tarefa" => $this->gettarefa(),
+            ":descricao" => $this->getdescricao(),
+            ":data_inicio_tarefa" => $this->getdata_inicio_tarefa(),
+            ":data_fim_tarefa" => $this->getdata_fim_tarefa(),
+        ));
+
     }
 
     public function get($id_projeto)
@@ -59,10 +90,13 @@ class Projetos extends Model
 
         $sql = new Sql();
 
-        $results = $sql->select("SELECT * FROM tb_projetos WHERE id_projeto = :id_projeto", [
-            ':id_projeto' => $id_projeto,
+        $results = $sql->select("SELECT * FROM tb_projetos a
+                INNER JOIN tb_client b ON b.idclient = a.idclient
+                INNER JOIN tb_employees c ON c.id_employees = a.id_employees
+                WHERE a.id_projeto = :id_projeto
+                ", [
+            ':id_projeto' => $id_projeto
         ]);
-
         $this->setData($results[0]);
     }
 
@@ -81,18 +115,18 @@ class Projetos extends Model
 
         $sql = new Sql();
 
-        $results = $sql->select("CALL sp_update_product(:id_product, :color, :category_name, :brand, :size, :description, :bar_code, :price, :qtdproduct)", array(
-            ":id_product" => $this->getid_product(),
-            ":color" => $this->getcolor(),
-            ":category_name" => $this->getcategory_name(),
-            ":brand" => $this->getbrand(),
-            ":size" => $this->getsize(),
-            ":description" => $this->getdescription(),
-            ":bar_code" => $this->getbar_code(),
-            ":price" => $this->getprice(),
-            ":qtdproduct" => $this->getqtdproduct(),
+        $results = $sql->select("CALL sp_update_projetos(:id_projeto, :nome_projeto, :idclient,
+        :id_employees, :descricao_projeto, :estado, :progresso, :orcamento_projeto, :data_inicio_projeto, :data_fim_projeto)", array(
+            ":id_projeto" => $this->getid_projeto(),
+            ":nome_projeto" => $this->getnome_projeto(),
+            ":idclient" => $this->getidclient(),
+            ":id_employees" => $this->getid_employees(),
+            ":descricao_projeto" => $this->getdescricao_projeto(),
+            ":estado" => $this->getestado(),
+            ":progresso" => $this->getprogresso(),
+            ":orcamento_projeto" => $this->getorcamento_projeto(),
+            ":data_inicio_projeto" => $this->getdata_inicio_projeto(),
+            ":data_fim_projeto" => $this->getdata_fim_projeto(),
         ));
-
-        // $this->setData($results[0]);
     }
 }
